e0a89decd9c0c0cf86f0ad834f383ce17c4a3fc3
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-04 17:19:37 -0400

    added apollo provider and implemented register mutation in view

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import { Routes } from "./Routes";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Routes />, document.getElementById("root") as HTMLElement);
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
 registerServiceWorker();
--- src/module/user/RegisterView.tsx
+++ src/module/user/RegisterView.tsx
@@ -1,5 +1,15 @@
 import * as React from "react";
 import PureComponent = React.PureComponent;
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+
+// TODO implement types with apollo-cli
+
+const registerMutation = gql`
+  mutation RegisterMutation($email: String!, $password: String!) {
+    register(email: $email, password: $password)
+  }
+`;
 
 export class RegisterView extends PureComponent {
   state = {
@@ -17,38 +27,42 @@ export class RegisterView extends PureComponent {
   render() {
     const { password, email } = this.state;
     return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <button onClick={() => console.log("button pressed")}>
-            register
-          </button>
-        </div>
-      </div>
+      <Mutation mutation={registerMutation}>
+        {mutate => (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <div>
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <button onClick={() => console.log("button pressed")}>
+                register
+              </button>
+            </div>
+          </div>
+        )}
+      </Mutation>
     );
   }
 }
