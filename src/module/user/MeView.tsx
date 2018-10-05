afd7aa90c2a8d579a79d18fccf4cdee54873a386
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-05 13:06:51 -0400

    created me view(logged in view) and generated schema types

--- src/module/user/MeView.tsx
+++ src/module/user/MeView.tsx
@@ -1,9 +1,39 @@
 import * as React from "react";
 import PureComponent = React.PureComponent;
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import { MeQuery } from "../../schemaTypes";
+
+const meQuery = gql`
+  query MeQuery {
+    me {
+      id
+      email
+    }
+  }
+`;
 
 export class MeView extends PureComponent {
   state = {};
   render() {
-    return <div>Me View</div>;
+    return (
+      <Query<MeQuery> query={meQuery}>
+        {({ data, loading }) => {
+          if (loading) {
+            return null;
+          }
+
+          if (!data) {
+            return <div>data is undefined</div>;
+          }
+
+          if (!data.me) {
+            return <div>received no user</div>;
+          }
+
+          return <div>{data.me.email}</div>;
+        }}
+      </Query>
+    );
   }
 }
