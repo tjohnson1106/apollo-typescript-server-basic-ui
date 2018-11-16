d5e64381ae494ea7be1d946afe25afbb0dd8d01b
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-16 12:46:00 -0500

    apollo created custom  render testing mock up

---
+++ src/testing-utils/customRender.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
+import { Router, Switch } from "react-router-dom";
+import { createMemoryHistory } from "history";
+
+export const customRender = (
+  node: JSX.Element | null,
+  mocks?: MockedResponse[],
+  {
+    route = "/",
+    history = createMemoryHistory({
+      initialEntries: [route]
+    })
+  } = {}
+) => {
+  return {
+    ...render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Router history={history}>
+          <Switch>{node}</Switch>
+        </Router>
+      </MockedProvider>
+    )
+  };
+};
